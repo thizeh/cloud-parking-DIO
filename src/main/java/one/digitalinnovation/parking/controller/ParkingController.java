1744df5783ebd64e313e93b468c87c78c77e375a
thizeh <thiago02_08@hotmail.com>
2022-08-05 09:25:54 -0300

    teste /parking

---
+++ src/main/java/one/digitalinnovation/parking/controller/ParkingController.java
@@ -0,0 +1,28 @@
+package one.digitalinnovation.parking.controller;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import one.digitalinnovation.parking.model.Parking;
+
+@RestController
+@RequestMapping("/parking")
+public class ParkingController {
+
+	@GetMapping
+	public List<Parking> findAll(){
+		
+		var parking = new Parking();
+		parking.setColor("PRETO");
+		parking.setLicense("MSS-1111");
+		parking.setModel("VW GOL");
+		parking.setState("SP");
+		
+		return Arrays.asList(parking);
+	}
+	
+}
