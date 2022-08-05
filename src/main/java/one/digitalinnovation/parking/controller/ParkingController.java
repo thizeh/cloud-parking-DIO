a938469f54ef0268a93867a96c2193ed8ebb8a5d
thizeh <thiago02_08@hotmail.com>
2022-08-05 15:23:39 -0300

    requisicao post /parking

--- src/main/java/one/digitalinnovation/parking/controller/ParkingController.java
+++ src/main/java/one/digitalinnovation/parking/controller/ParkingController.java
@@ -2,10 +2,16 @@ package one.digitalinnovation.parking.controller;
 
 import java.util.List;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import one.digitalinnovation.parking.controller.dto.ParkingCreateDTO;
 import one.digitalinnovation.parking.controller.dto.ParkingDTO;
 import one.digitalinnovation.parking.controller.mapper.ParkingMapper;
 import one.digitalinnovation.parking.model.Parking;
@@ -22,12 +28,27 @@ public class ParkingController {
 		this.parkingService = parkingService;
 		this.parkingMapper = parkingMapper;
 	}
-	
+		
 	@GetMapping
-	public List<ParkingDTO> findAll(){
+	public ResponseEntity<List<ParkingDTO>> findAll(){
 		List<Parking> parkingList = parkingService.findAll();
 		List<ParkingDTO> result = parkingMapper.toParkingDTOList(parkingList);
-		return result;
+		return ResponseEntity.ok(result);
 	}
+	
+	@GetMapping("/{id}")
+	public ResponseEntity<ParkingDTO> findById(@PathVariable String id){
+		Parking parking = parkingService.findById(id);
+		ParkingDTO result = parkingMapper.toParkingDTO(parking);
+		return ResponseEntity.ok(result);
+	}
+	
+	@PostMapping
+	public ResponseEntity<ParkingDTO> create(@RequestBody ParkingCreateDTO dto){
+		var parkingCreate = parkingMapper.toParkingCreate(dto);
+		Parking parking = parkingService.create(parkingCreate);
+		ParkingDTO result = parkingMapper.toParkingDTO(parking);
+		return ResponseEntity.status(HttpStatus.CREATED).body(result);
+	}	
 }
 
